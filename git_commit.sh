f04ad3a6e179048e6d4c74440b3b49d8054db70e
Githubinspiration <zangming0412@gmail.com>
2024-06-12 11:13:34 +0900

    finish

--- git_commit.sh
+++
@@ -1,3 +0,0 @@
-git filter-branch --env-filter "export GIT_COMMITTER_NAME='Githubinspiration' export GIT_COMMITTER_EMAIL='zangming0412@gmail.com' export GIT_AUTHOR_NAME='Githubinspiration' export GIT_AUTHOR_EMAIL='zangming0412@gmail.com'" --tag-name-filter cat -- --branches --tags
-git push --all
-git push --tags
